bf743c84a525f96cfab8e7c52a20602d0f689e90
Simon-Mategen <simon.mategen@gmail.com>
2022-05-05 15:05:20 +0200

    Added new Jest test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,11 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop should remove top element', () =>{
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(2);
+});
